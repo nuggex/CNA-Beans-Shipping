5ea76737966fbc500920c59f46e787b613ebb548
Marwwin <markus.kankkonen@anvianet.fi>
2021-01-25 18:44:57 +0200

    dd

--- models/order.js
+++ models/order.js
@@ -9,7 +9,7 @@ const orderSchema = mongoose.Schema({
   town: { type: String, required: true },
   state: { type: String, required: true },
   date: { type: Date, required: true },
-  phoneNumber: { type: Number, required: true },
+  phoneNumber: { type: String, required: true },
   zipCode: { type: Number, required: true },
   tracking: {type: String, required: true }
 });
