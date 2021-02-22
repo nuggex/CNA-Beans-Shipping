42c11200882fcdd9c4d3fa72798f46ceb9aa7047
nuggex <nuggex@gmail.com>
2021-02-22 14:32:08 +0200

    testing

--- routes/orders.js
+++ routes/orders.js
@@ -16,6 +16,7 @@ router.get("/", (req, res, next) => {
   Order.find()
     .exec()
     .then(data => {
+      console.log(data);
       let newData = data.map(x => {
         let newOrder = {};
         for (let [k, v] of Object.entries(x)) { if (k != "_id") { newOrder[k] = v } }
